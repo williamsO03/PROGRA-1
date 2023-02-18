bfc136e04d34a8b7782c3351bb5364c28809eb22
williamsO03 <125751090+williamsO03@users.noreply.github.com>
2023-02-18 09:19:53 -0600

    Add files via upload

---
+++ Semana4/Entidades/Contar_Caracteres.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Semana4
+{
+    public class Contar_Caracteres
+    {
+        public int Letras(string letras)
+        {
+            int caracteres = letras.Length;
+            return caracteres;
+        }
+    }
+}
---
+++ Semana4/Program.cs
@@ -0,0 +1,11 @@
+﻿using Semana4;
+
+Contar_Caracteres letras = new Contar_Caracteres();
+
+Console.WriteLine("Ingresa los caracteres que desees");
+
+string Texto = Console.ReadLine();
+
+int numeros = letras.Letras(Texto);
+
+Console.WriteLine("La cantidad de caracteres de tu texto es de " + numeros);
